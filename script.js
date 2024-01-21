1ea52afae94f3a6019496b9d223677b9173ac61a
SeyemAhamed <seyamjh@gmail.com>
2024-01-21 18:00:08 +0600

    Javascript foreach, while & do while loop

---
+++ script.js
@@ -0,0 +1,24 @@
+let numbers = [50, 40, 84, 30, 52];
+
+// for(i=0; i<=4; i++){
+//     console.log(numbers[i]);
+// }
+
+// numbers.forEach(elements=>{
+//     console.log(elements);
+// });
+
+// let i=0;
+// while(i<=4){
+//     console.log(numbers[i]);
+//     i++;
+// }
+
+let j=0;
+
+do{
+    console.log(numbers[j]);
+    j++;
+}
+
+while(j<=4);
